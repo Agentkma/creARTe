63e4ba5dc74acc69ca31b48cf4d8a325ebbde5c0
KevinMAnderson <kma2k4@hotmail.com>
2018-01-20 17:14:32 -0700

    sign in and up modal toggle working and tied Sign In to username state so when signed in this will change to user name

--- client/src/Actions/types.js
+++ client/src/Actions/types.js
@@ -1,1 +1,2 @@
 export const SELECTED_ART = 'selected_art';
+export const USER_SIGN_IN = 'user_sign_in';
--- client/src/Components/ModalSignLogin.js
+++ client/src/Components/ModalSignLogin.js
@@ -3,16 +3,20 @@
 import React from 'react';
 import { NavLink, Modal, ModalBody, Fade } from 'reactstrap';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { signIn } from '../Actions';
 
 class ModalSignLogin extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			modal: false,
-			cardStyle: 'card:not(signIn)'
+			showSignIn: true
 		};
 
 		this.toggleModal = this.toggleModal.bind(this);
+		this.renderSignInOrSignUp = this.renderSignInOrSignUp.bind(this);
+		this.flipCard = this.flipCard.bind(this);
 	}
 
 	toggleModal() {
@@ -23,26 +27,154 @@ class ModalSignLogin extends React.Component {
 	}
 
 	flipCard() {
-		this.setState({});
+		const { showSignIn } = this.state;
+		this.setState({ showSignIn: !showSignIn });
 	}
 
-	reverseFlipCard() {}
+	renderSignInOrSignUp() {
+		const { showSignIn } = this.state;
+		if (showSignIn) {
+			return (
+				<div className="signIn panel panel-default">
+					<form className="form-horizontal">
+						<br />
 
-	flipperCard = e => {
-		e.preventDefault();
-		this.setState({ cardStyle: 's signUp)' });
-	};
+						<h1 className="text-center">
+							cre<span className="createorlog1">ART</span>ivity
+						</h1>
+						<br />
+						<br />
 
-	flipper2 = e => {
-		e.preventDefault();
-		this.setState({ cardStyle: 'card' });
-	};
+						<input
+							className="form-control"
+							placeholder="Email"
+							type="email"
+							minLength="6"
+							maxLength="20"
+							required
+						/>
+						<input
+							className="form-control"
+							placeholder="Password"
+							type="password"
+							minLength="6"
+							maxLength="20"
+							required
+						/>
+						<button
+							className="btn btn-primary btn-block"
+							type="submit"
+						>
+							LOG IN
+						</button>
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+						<p className="text-center">
+							<a
+								href="#"
+								className="fliper-btn createorlog"
+								onClick={this.flipCard}
+							>
+								Create new account?
+							</a>
+						</p>
+					</form>
+				</div>
+			);
+		} else {
+			return (
+				<div className="  signUp face back">
+					<div className="panel panel-default">
+						<form
+							className="form-horizontal"
+							data-toggle="validator"
+							role="form"
+						>
+							<br />
+							<h1 className="text-center">
+								cre<span>ART</span>e
+							</h1>
+							<br />
+							<label>Basic Information</label>
+							<input
+								className="form-control"
+								placeholder="Full name"
+								minLength="6"
+								maxLength="20"
+								type="text"
+								required
+							/>
+							<input
+								className="form-control"
+								placeholder="Email"
+								type="email"
+								minLength="6"
+								maxLength="20"
+								required
+							/>
+							<label>Private Information</label>
+							<input
+								type="password"
+								className="form-control"
+								placeholder="Password (min 6 characters)"
+								minLength="6"
+								maxLength="20"
+								id="inputPassword"
+								required
+							/>
+							<input
+								type="password"
+								className="form-control"
+								minLength="6"
+								maxLength="20"
+								placeholder="Confirm Password"
+								data-match="#inputPassword"
+								data-match-error="Whoops, these don't match"
+								required
+							/>
+							<button
+								className="btn btn-primary btn-block"
+								type="submit"
+							>
+								SIGN UP
+							</button>
+							<br />
+							<p className="text-center">
+								<a
+									href="#"
+									className="fliper-btn createorlog"
+									onClick={this.flipCard}
+								>
+									Already have an account?
+								</a>
+							</p>
+						</form>
+					</div>
+				</div>
+			);
+		}
+	}
+
+	// flipperCard(e) {
+	// 	e.preventDefault();
+	// 	this.setState({ cardStyle: 'cardSup:not(.signUp))' });
+	// }
+	//
+	// flipper2(e) {
+	// 	e.preventDefault();
+	// 	this.setState({ cardStyle: 'cardSin:not(.signUp) ' });
+	// }
 
 	render() {
-		const { modal, cardStyle } = this.state;
+		const { modal } = this.state;
 		return (
 			<NavLink onClick={this.toggleModal} href="#">
-				Sign Up/In
+				{this.props.username}
 				<Modal
 					isOpen={modal}
 					toggle={this.toggleModal}
@@ -52,140 +184,7 @@ class ModalSignLogin extends React.Component {
 						<div className="container">
 							<div className="row">
 								<div className="col">
-									<div className="flip">
-										<div className={cardStyle}>
-											<div className="signIn panel panel-default">
-												<form className="form-horizontal">
-													<br />
-
-													<h1 className="text-center">
-														cre<span className="createorlog1">
-															ART
-														</span>ivity
-													</h1>
-													<br />
-													<br />
-
-													<input
-														className="form-control"
-														placeholder="Email"
-														type="email"
-														minLength="6"
-														maxLength="20"
-														required
-													/>
-													<input
-														className="form-control"
-														placeholder="Password"
-														type="password"
-														minLength="6"
-														maxLength="20"
-														required
-													/>
-													<button
-														className="btn btn-primary btn-block"
-														type="submit"
-													>
-														LOG IN
-													</button>
-													<br />
-													<br />
-													<br />
-													<br />
-													<br />
-													<br />
-													<br />
-													<p className="text-center">
-														<a
-															href="#"
-															className="fliper-btn createorlog"
-															onClick={this.flipCard.bind(
-																this
-															)}
-														>
-															Create new account?
-														</a>
-													</p>
-												</form>
-											</div>
-
-											<div className="signUp face back">
-												<div className="panel panel-default">
-													<form
-														className="form-horizontal"
-														data-toggle="validator"
-														role="form"
-													>
-														<br />
-														<h1 className="text-center">
-															cre<span>ART</span>e
-														</h1>
-														<br />
-														<label>
-															Basic Information
-														</label>
-														<input
-															className="form-control"
-															placeholder="Full name"
-															minLength="6"
-															maxLength="20"
-															type="text"
-															required
-														/>
-														<input
-															className="form-control"
-															placeholder="Email"
-															type="email"
-															minLength="6"
-															maxLength="20"
-															required
-														/>
-														<label>
-															Private Information
-														</label>
-														<input
-															type="password"
-															className="form-control"
-															placeholder="Password (min 6 characters)"
-															minLength="6"
-															maxLength="20"
-															id="inputPassword"
-															required
-														/>
-														<input
-															type="password"
-															className="form-control"
-															minLength="6"
-															maxLength="20"
-															placeholder="Confirm Password"
-															data-match="#inputPassword"
-															data-match-error="Whoops, these don't match"
-															required
-														/>
-														<button
-															className="btn btn-primary btn-block"
-															type="submit"
-														>
-															SIGN UP
-														</button>
-														<br />
-														<p className="text-center">
-															<a
-																href="#"
-																className="fliper-btn createorlog"
-																onClick={this.reverseFlipCard.bind(
-																	this
-																)}
-															>
-																Already have an
-																account?
-															</a>
-														</p>
-													</form>
-												</div>
-											</div>
-										</div>
-									</div>
+									{this.renderSignInOrSignUp()}
 								</div>
 							</div>
 						</div>
@@ -240,4 +239,14 @@ Modal.propTypes = {
 	modalTransition: PropTypes.shape(Fade.propTypes)
 };
 
-export default ModalSignLogin;
+// pass state or state props to component via connect(mapStateToProps)
+// user comes from key name in index.js from Reducer....other vars are from this reducer state
+const mapStateToProps = ({ user }) => {
+	const { username } = user;
+
+	return {
+		username
+	};
+};
+//connect action creators { } to ModalSignLogin component
+export default connect(mapStateToProps, { signIn })(ModalSignLogin);
